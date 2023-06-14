061a982e53dd050a8a456c7d767db1bf5a14d5da
computor IT <computor IT@DESKTOP-P51CF04>
2023-06-14 10:24:42 +0700

    Add project files.

---
+++ ConsoleApp_4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Wananchai
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            for (int iterator = 0; iterator < 3; iterator++)
+            {
+                Console.WriteLine("Outer Loop:" + iterator);//Execute 3 time
+
+                for (int _iterator = 0; _iterator < 2; _iterator++)
+                {
+                    Console.WriteLine("Inner Loop:" + iterator);//Excute 2 time
+
+                }
+
+
+            }
+
+            string[] brandTV = { "LG", "SONY", "SAMSUNG", "TCL", "JVC" };
+
+            foreach (string television in brandTV)
+            {
+                Console.WriteLine("Brand" + television);
+            }
+        }
+    }
+}
